304068db078cc1a86d61cfcb1064b2c3663ca239
avvinci <vinciabhinav7@gmail.com>
2019-04-27 17:19:50 +0530

    trying flexbox cards

--- scripts.js
+++ scripts.js
@@ -1,19 +1,11 @@
-function readTextFile(file)
-{
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var allText = rawFile.responseText;
-                console.log(allText);
-            }
-        }
-    }
-    rawFile.send(null);
-}
-
-readTextFile("mytext.txt");
+var openFile = function(event) {
+    var input = event.target;
+    var reader = new FileReader();
+    reader.onload = function(){
+      var text = reader.result;
+      var node = document.getElementById('output');
+      node.innerText = text;
+      console.log(reader.result.substring(0, 200));
+    };
+    reader.readAsText(input.files[0]);
+  };
